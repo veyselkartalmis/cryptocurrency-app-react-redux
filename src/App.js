d9c1e62a29a853cef5d9feeafdf596f1e91579bf
Veysel Kartalmis <veyselkartalms@gmail.com>
2022-07-22 18:07:22 +0300

    starter template

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+
+const App = () => {
+	return (
+        <div className="app">
+            <div className="navbar">
+            </div>
+            <div className="main">
+            </div>
+            <div className="footer">
+            </div>
+        </div>
+	);
+};
+
+export default App;
+
+
+{/* <BrowserRouter>
+			<Routes>
+				<Route exact path="/"></Route>
+			</Routes>
+		</BrowserRouter> */}
