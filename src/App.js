06da7a709208f5c42871f8b4ccd101661a4ed77e
Veysel Kartalmis <veyselkartalms@gmail.com>
2022-07-22 18:33:15 +0300

    navbar added

--- src/App.js
+++ src/App.js
@@ -2,24 +2,26 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Layout, Typography, Space } from "antd";
 
+import { Navbar } from "./components";
+
 const App = () => {
 	return (
-        <div className="app">
-            <div className="navbar">
-            </div>
-            <div className="main">
-            </div>
-            <div className="footer">
-            </div>
-        </div>
+		<div className="app">
+			<div className="navbar">
+				<Navbar />
+			</div>
+			<div className="main"></div>
+			<div className="footer"></div>
+		</div>
 	);
 };
 
 export default App;
 
-
-{/* <BrowserRouter>
+{
+	/* <BrowserRouter>
 			<Routes>
 				<Route exact path="/"></Route>
 			</Routes>
-		</BrowserRouter> */}
+		</BrowserRouter> */
+}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { Button, Menu, Typography, Avatar } from "antd";
+import { NavLink } from "react-router-dom";
+import {
+	HomeOutlined,
+	MoneyCollectOutlined,
+	BulbOutlined,
+	FundOutlined,
+	MenuOutlined,
+} from "@ant-design/icons";
+
+import icon from "../images/cryptocurrency.png";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+	return (
+		<div className="nav-container">
+			<div className="logo-container">
+				<Avatar src={icon} size="large" />
+				<Typography.Title level={2} className="logo">
+					<a>Cryptoverse</a>
+				</Typography.Title>
+			</div>
+		</div>
+	);
+};
 
-export default Navbar
+export default Navbar;
