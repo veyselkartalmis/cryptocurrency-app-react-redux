51d7aab44549ca6ce3657e691e0c6bd1ae085dac
Veysel Kartalmis <veyselkartalms@gmail.com>
2022-07-27 18:55:49 +0300

    cryptoApi update

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,10 +1,20 @@
-import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const options = {
-    method: 'GET',
-    url: 'https://coinranking1.p.rapidapi.com/coins',
-    headers: {
-      'X-RapidAPI-Key': 'd67c1bd16amsh9c9b01c97fdef5dp1f5509jsn76522c675d09',
-      'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
-    }
-  };
+const cryptoApiHeaders = {
+	"X-RapidAPI-Key": "d67c1bd16amsh9c9b01c97fdef5dp1f5509jsn76522c675d09",
+	"X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+};
+
+const baseUrl = "https://coinranking1.p.rapidapi.com";
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+	reducerPath: "cryptoApi",
+	baseQuery: fetchBaseQuery({ baseUrl }),
+	endpoints: (builder) => ({
+		query: () => createRequest("/exchanges"),
+	}),
+});
+
+export const { useGetCryptosQuery } = cryptoApi;
