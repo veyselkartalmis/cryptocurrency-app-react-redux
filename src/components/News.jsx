551e6138377f9c08b7a63d4739a873dfe30d826c
Veysel Kartalmis <veyselkartalms@gmail.com>
2022-07-29 19:03:19 +0300

    bug

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -10,10 +10,29 @@ const { Option } = Select;
 
 const News = ({ simplified }) => {
 	const { data: cryptoNews } = useGetCryptoNewsQuery({
-		newsCategory: "Cryptocurrencies",
-		count: simplified ? 10 : 100,
+		newsCategory: "Cryptocurrency",
+		count: simplified ? 6 : 12,
 	});
-	return <></>;
+
+  console.log(cryptoNews);
+
+	if (!cryptoNews?.value) return "Loading...";
+
+	return (
+		<Row gutter={[24, 24]}>
+			{cryptoNews?.value.map((news, i) => (
+				<Col xs={24} sm={12} lg={8} key={i}>
+					<Card hoverable className="news-card">
+						<a href={news.url} target="_blank" rel="noreferrer">
+							<div className="news-image-container">
+								<Title className="news-title" level={4}>{news.name}</Title>
+							</div>
+						</a>
+					</Card>
+				</Col>
+			))}
+		</Row>
+	);
 };
 
 export default News;
