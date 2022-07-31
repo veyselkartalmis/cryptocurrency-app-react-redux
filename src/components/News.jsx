01a78daefec4cb2d6345bac6d8079605f55678d8
Veysel <veyselkartalms@gmail.com>
2022-07-31 23:55:30 +0300

    news added+

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -8,10 +8,12 @@ import { useGetCryptoNewsQuery } from "../services/cryptoNewsApi";
 const { Text, Title } = Typography;
 const { Option } = Select;
 
+const demoImage = "https://www.cumhuriyet.com.tr/Archive/2022/5/21/1938309/kapak_111958.jpg";
+
 const News = ({ simplified }) => {
 	const { data: cryptoNews } = useGetCryptoNewsQuery({
 		newsCategory: "Cryptocurrency",
-		count: simplified ? 10 : 100
+		count: simplified ? 6 : 12
 	});
 
 	return (
@@ -22,6 +24,17 @@ const News = ({ simplified }) => {
 						<a href={news.url} target="_blank" rel="noreferrer">
 							<div className="news-image-container">
 								<Title className="news-title" level={4}>{news.name}</Title>
+								<img style={{ maxWidth: "200px", maxHeight: "100px" }} src={news?.image?.thumbnail?.contentUrl} alt="news" />
+							</div>
+							<p>
+								{news?.description > 100 ? `${news?.description.substring(0, 100)}...` : news?.description}
+							</p>
+							<div className="provider-container">
+								<div>
+									<Avatar src={news?.provider[0]?.image?.thumbnail?.contentUrl} alt="news" />
+									<Text className="provider-name">{news?.provider[0]?.name}</Text>
+								</div>
+								<Text>{moment(news.datePublished).startOf("ss").fromNow()}</Text>
 							</div>
 						</a>
 					</Card>
