17b2a3db367bc40da9b787b6d87bd8e858f679a6
Veysel Kartalmis <veyselkartalms@gmail.com>
2022-08-04 18:40:36 +0300

    getCryptoHistory services added

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -62,7 +62,7 @@ const CryptoDetails = () => {
           {time.map((date) => <Option key={date}>{date}</Option>)}
         </Select>
 
-        <LineChart/>
+        <LineChart coinHistory={coinHistory} currentPrice={millify(cryptoDetails?.price)} coinName={cryptoDetails?.name}/>
 
         <Col className="stats-container"> 
           <Col className="coin-value-statistics">
